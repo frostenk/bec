f5e40c5b8e4685d0c2ca130d6434aeb5752fbe44
Frostenk <98924876+frostenk@users.noreply.github.com>
2026-01-09 20:26:21 -0400

    delete the awesome config, it's outdated anyways

--- awesome/ui/notification/init.lua
+++
@@ -1,8 +0,0 @@
-local naughty = require('naughty')
-
-return function(n)
-   return naughty.layout.box({ 
-     notification = n,
-     position     = "bottom_right",
-   })
-end
--- awesome/ui/wibar/module/separator.lua
+++
@@ -1,8 +0,0 @@
-local awful = require('awful')
-local wibox = require('wibox')
-
-local separator = wibox.widget({
-		widget = wibox.widget.separator,
-		forced_width = 10,
-		opacity = 0,
-	})
